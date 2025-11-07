76409f7891999fcb5397c24882dde42c0bceb65a
hai-nguyen1112 <hainguyen871112@gmail.com>
2025-11-07 12:20:24 -0500

    Added SignIn, SignOut

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -1,14 +1,19 @@
+import { signInAction } from '@/app/_lib/actions';
+
 function SignInButton() {
+  // Because SignInButton is a server component, we cannot do onClick. Instead we wrap the button inside a form and use server actions
   return (
-    <button className='flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium'>
-      <img
-        src='https://authjs.dev/img/providers/google.svg'
-        alt='Google logo'
-        height='24'
-        width='24'
-      />
-      <span>Continue with Google</span>
-    </button>
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+        <img
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span>Continue with Google</span>
+      </button>
+    </form>
   );
 }
 
