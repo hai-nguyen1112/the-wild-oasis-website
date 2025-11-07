9edbb7b051698966ef70d3186c704119101465a1
hai-nguyen1112 <hainguyen871112@gmail.com>
2025-11-07 12:41:35 -0500

    Created Guest

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { createGuest, getGuest } from './data-service';
 
 const authConfig = {
   providers: [
@@ -10,7 +11,28 @@ const authConfig = {
   ],
   callbacks: {
     authorized({ auth, request }) {
+      // This function is called right before the protected route is rendered
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      // This funcion is called after the user enters credentials but before the user session is created (actually logged in)
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) {
+          await createGuest({ email: user.email, fullName: user.name });
+        }
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      // This function is called right after the user is logged in
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
--- middleware.js
+++ middleware.js
@@ -6,6 +6,8 @@
 //   return NextResponse.redirect(new URL('/about', request.url));
 // }
 import { auth } from '@/app/_lib/auth';
+
+// This middleware will be called when the requested route matches the matcher. The auth middleware will run the authorized callback in authConfig in auth.js
 export const middleware = auth;
 
 export const config = {
