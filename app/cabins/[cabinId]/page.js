7db19dc114117f4c7d735797401659b711be00cc
hai-nguyen1112 <hainguyen871112@gmail.com>
2025-11-10 14:12:00 -0500

    Made Reserve Text Responsive

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -25,7 +25,7 @@ export default async function Page({ params }) {
     <div className="max-w-6xl mx-auto mt-8">
       <Cabin cabin={cabin} />
       <div>
-        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-center mb-10 text-accent-400">
           Reserve {cabin.name} today. Pay on arrival.
         </h2>
 
